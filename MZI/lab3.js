e04a1289fa223777a5b8339bb665bdab84fc26a3
gaykov <sdgaykov@gmail.com>
2017-09-14 20:09:09 +0300

    Fix AES algorithm

--- MZI/lab3.js
+++ MZI/lab3.js
@@ -135,17 +135,9 @@ function AES_MixColumns_Inv(state) {
 }
 
 
-AES_Init();
-const block = ['a', 'b', 'c'].map(char => char.charCodeAt(0));
-
- const key = new Array(32);
-for(let i = 0; i < 32; i++)
-      key[i] = i;
-
-AES_ExpandKey(key);
-console.log(block);
-AES_Encrypt(block, key);
-console.log(block);
-AES_Decrypt(block, key);
-console.log(block.map(char => String.fromCharCode(char)));
-// AES_Done();
+module.exports = {
+  init: AES_Init,
+  expandKey: AES_ExpandKey,
+  encrypt: AES_Encrypt,
+  decrypt: AES_Decrypt,
+};
--- MZI/server.js
+++ MZI/server.js
@@ -1,14 +1,18 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
 const CaesarShift = require('./lab1');
+const AES = require('./lab3');
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
+AES.init();
+
 io.on('connection', function(socket){
+  // LAB1 --- CAESAR -------
   socket.on('caesarEncrypt', function(data){
     const { text, shift } = JSON.parse(data);
     socket.emit('caesarResult', CaesarShift(text, parseInt(shift, 10)));
@@ -18,6 +22,51 @@ io.on('connection', function(socket){
     const { text, shift } = JSON.parse(data);
     socket.emit('caesarResult', CaesarShift(text, -1 * parseInt(shift, 10)));
   });
+  // -------------------------
+
+
+  // LAB3 ------- AES ---------
+  socket.on('aesEncrypt', function(data){
+    let { text, key } = JSON.parse(data);
+
+    text = text.split('').map(char => char.charCodeAt(0));
+    key = key.split('').map(char => char.charCodeAt(0));
+
+    if (key.length < 32) {
+      for(let i = key.length; i < 32; i++) key.push(0);
+    } else if (key.length < 64) {
+      for(let i = key.length; i < 64; i++) key.push(0);
+    } else if (key.length < 128) {
+      for(let i = key.length; i < 128; i++) key.push(0);
+    }
+
+    AES.expandKey(key);
+    AES.encrypt(text, key);
+
+    socket.emit('aesResult', text);
+  });
+
+  socket.on('aesDecrypt', function(data){
+    let { text, key } = JSON.parse(data);
+
+    text = text.split(',');
+    key = key.split('').map(char => char.charCodeAt(0));
+
+    if (key.length < 32) {
+      for(let i = key.length; i < 32; i++) key.push(0);
+    } else if (key.length < 64) {
+      for(let i = key.length; i < 64; i++) key.push(0);
+    } else if (key.length < 128) {
+      for(let i = key.length; i < 128; i++) key.push(0);
+    }
+
+    AES.expandKey(key);
+    AES.decrypt(text, key);
+    text = text.map(char => String.fromCharCode(char)).join('');
+
+    socket.emit('aesResult', text);
+  });
+  // ----------------------------
 });
 
 http.listen(3000, function(){
